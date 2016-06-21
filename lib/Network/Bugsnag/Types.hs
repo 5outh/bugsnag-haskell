168ac3fcce2a57b1c992464c739ffa57e92917ad
Benjamin Kovach <bkovach5@uga.edu>
2016-06-21 09:27:07 -0400

    Cleanup

---
+++ lib/Network/Bugsnag/Types.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Network.Bugsnag.Types where
+
+import Data.Text
+import GHC.Generics
+import Data.Aeson
+
+text :: Text -> Text
+text t = t
+
+data BugsnagEvent = BugsnagEvent
+  { exceptions :: [BugsnagException]
+  , threads :: Maybe [BugsnagThread]
+  , context :: Maybe Text 
+  , groupingHash :: Maybe Text
+  , severity :: Maybe Text
+  , user :: BugsnagUser 
+  , app :: BugsnagApp 
+  , device :: BugsnagDevice
+  , metaData :: Maybe Value 
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagEvent where
+  toJSON BugsnagEvent{..} = object
+    [ "payloadVersion" .= text "2"
+    , "exceptions" .= toJSON exceptions
+    , "threads" .= toJSON threads
+    , "context" .= toJSON context
+    , "groupingHash" .= toJSON groupingHash 
+    , "severity" .= toJSON severity 
+    , "user" .= toJSON user 
+    , "app" .= toJSON app 
+    , "device" .= toJSON device 
+    , "metaData" .= toJSON metaData 
+    ]
+
+data BugsnagThread = BugsnagThread
+  { threadId :: Text
+  , threadName :: Text
+  , threadStackTrace :: [BugsnagStackFrame]
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagThread where
+  toJSON BugsnagThread{..} = object
+    [ "id" .= threadId
+    , "name" .= threadName
+    , "stacktrace" .= toJSON threadStackTrace
+    ]
+
+data BugsnagStackFrame = BugsnagStackFrame
+  { file :: Text
+  , method :: Text
+  , lineNumber :: Int
+  , columnNumber :: Maybe Int
+  , inProject :: Bool
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagStackFrame
+
+data BugsnagException = BugsnagException
+  { errorClass :: Text
+  , message :: Text
+  , stacktrace :: [BugsnagStackFrame]
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagException
+
+data BugsnagUser = BugsnagUser
+  { id :: Maybe Text
+  , name :: Maybe Text
+  , email :: Maybe Text
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagUser
+
+data BugsnagApp  = BugsnagApp
+  { version :: Maybe Text
+  , releaseStage :: Maybe Text
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagApp
+
+data BugsnagDevice = BugsnagDevice
+  { osVersion :: Maybe Text
+  , hostname :: Maybe Text
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagDevice
+
+data BugsnagRequest = BugsnagRequest
+  { apiKey :: Text 
+  , events :: [BugsnagEvent]
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON BugsnagRequest where
+  toJSON BugsnagRequest{..} =
+    object
+      [ "apiKey" .= apiKey
+      , "notifier" .= object
+        [ "name" .= text "Haskell Notifier"
+        , "version" .= text "1.0.1.1"
+        , "url" .= text "https://github.com/5outh/bugsnag-haskell"
+        ]
+      , "events" .= toJSON events
+      ]
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,48 +1,18 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
+import Network.Bugsnag.Types
 import System.Environment
 import Network.HTTP.Dispatch.Core
 import Network.HTTP.Dispatch.Types
 import Data.ByteString.Lazy as BL
 import Data.Text
 import qualified Data.Text as T
-import GHC.Generics
 import Data.Aeson
 
 appKey :: IO Text
 appKey = T.pack <$> getEnv "BUGSNAG_APP_KEY"
-
-data BugsnagEvent = BugsnagEvent
-  { exceptions :: [BugsnagException]
-  , threads :: Maybe [BugsnagThread]
-  , context :: Maybe Text 
-  , groupingHash :: Maybe Text
-  , severity :: Maybe Text
-  , user :: BugsnagUser 
-  , app :: BugsnagApp 
-  , device :: BugsnagDevice
-  , metaData :: Maybe Value 
-  } deriving (Show, Eq, Generic)
-
-
--- TODO: Finish testing
-instance ToJSON BugsnagEvent where
-  toJSON BugsnagEvent{..} = object
-    [ "payloadVersion" .= text "2"
-    , "exceptions" .= toJSON exceptions
-    , "threads" .= toJSON threads
-    , "context" .= toJSON context
-    , "groupingHash" .= toJSON groupingHash 
-    , "severity" .= toJSON severity 
-    , "user" .= toJSON user 
-    , "app" .= toJSON app 
-    , "device" .= toJSON device 
-    , "metaData" .= toJSON metaData 
-    ]
 
 testEvent :: BugsnagEvent
 testEvent = BugsnagEvent
@@ -55,103 +25,29 @@ testEvent = BugsnagEvent
   testApp
   testDevice
   Nothing
-
-data BugsnagThread = BugsnagThread
-  { threadId :: Text
-  , threadName :: Text
-  , threadStackTrace :: [BugsnagStackFrame]
-  } deriving (Show, Eq, Generic)
-
-instance ToJSON BugsnagThread where
-  toJSON BugsnagThread{..} = object
-    [ "id" .= threadId
-    , "name" .= threadName
-    , "stacktrace" .= toJSON threadStackTrace
-    ]
-
 testThread :: BugsnagThread
 testThread = BugsnagThread
   "2"
   "Main"
   []
 
-data BugsnagStackFrame = BugsnagStackFrame
-  { file :: Text
-  , method :: Text
-  , lineNumber :: Int
-  , columnNumber :: Maybe Int
-  , inProject :: Bool
-  } deriving (Show, Eq, Generic)
-
 testStackFrame :: BugsnagStackFrame
 testStackFrame = BugsnagStackFrame "Main.hs" "myReq" 88 (Just 0) True
-
-instance ToJSON BugsnagStackFrame
-
-data BugsnagException = BugsnagException
-  { errorClass :: Text
-  , message :: Text
-  , stacktrace :: [BugsnagStackFrame]
-  } deriving (Show, Eq, Generic)
 
 testException :: BugsnagException
 testException = BugsnagException "errorClass" "There was an error!" [testStackFrame]
 
-data BugsnagUser = BugsnagUser
-  { id :: Maybe Text
-  , name :: Maybe Text
-  , email :: Maybe Text
-  } deriving (Show, Eq, Generic)
-
 testUser :: BugsnagUser
 testUser = BugsnagUser (Just "10") (Just "Ben") (Just "ben@ben.com")
-
-data BugsnagApp  = BugsnagApp
-  { version :: Maybe Text
-  , releaseStage :: Maybe Text
-  } deriving (Show, Eq, Generic)
 
 testApp :: BugsnagApp
 testApp = BugsnagApp (Just "1.0") (Just "development")
 
-data BugsnagDevice = BugsnagDevice
-  { osVersion :: Maybe Text
-  , hostname :: Maybe Text
-  } deriving (Show, Eq, Generic)
-
 testDevice :: BugsnagDevice
 testDevice = BugsnagDevice (Just "1.0") (Just "web.io.com")
 
-instance ToJSON BugsnagException
-instance ToJSON BugsnagUser
-instance ToJSON BugsnagApp
-instance ToJSON BugsnagDevice
-
-data BugsnagRequest = BugsnagRequest
-  { apiKey :: Text 
-  , events :: [BugsnagEvent]
-  } deriving (Show, Eq, Generic)
-
 testRequest :: Text -> BugsnagRequest
 testRequest key = BugsnagRequest key [testEvent]
-
-text :: Text -> Text
-text t = t
-
-instance ToJSON BugsnagRequest where
-  toJSON BugsnagRequest{..} =
-    object
-      [ "apiKey" .= apiKey
-      , "notifier" .= object
-        [ "name" .= text "Haskell Notifier"
-        , "version" .= text "1.0.1.1"
-        , "url" .= text "https://github.com/5outh/bugsnag-haskell"
-        ]
-      , "events" .= toJSON events
-      ]
-
--- TODO: Move everything inside events, because that's where it lives for some
--- reason.
 
 myHttpReq :: Text -> HTTPRequest
 myHttpReq key = HTTPRequest
